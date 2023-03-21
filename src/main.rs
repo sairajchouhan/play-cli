d6d05051466304ddaf8366d33e465a5b1bc8fe38
sairaj <sairaj2119@gmail.com>
2023-03-21 19:12:31 +0530

    add clap

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ use std::{
     str::FromStr,
 };
 
-enum Action {
-    New,
-    Ls,
-}
+use clap::{Parser, Subcommand, ValueEnum};
 
+#[derive(ValueEnum, Clone, Debug)]
 enum Templates {
     TsNode,
     TsExpress,
@@ -43,84 +41,88 @@ impl FromStr for Templates {
     }
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    actions: Actions,
+}
+
+#[derive(Subcommand, Debug)]
+enum Actions {
+    New {
+        #[arg(value_enum)]
+        template: Templates,
+    },
+    Ls {
+        template: Option<Templates>,
+    },
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let action = env::args().nth(1);
-    let template = env::args().nth(2);
-    let target_dir = dirs::home_dir().expect("Error while getting home dir");
+    let target_dir = dirs::home_dir()
+        .expect("Error while getting home dir")
+        .join("playground");
 
-    let action = match action {
-        Some(item) => {
-            if item == "new" {
-                Action::New
-            } else if item == "ls" {
-                Action::Ls
-            } else {
-                panic!("now a valid action")
+    if !target_dir.exists() {
+        fs::create_dir(&target_dir).expect("Error in creating the target dir")
+    }
+
+    let cli = Cli::parse();
+
+    match cli.actions {
+        Actions::New { template } => {
+            let target_string = env::args()
+                .nth(3)
+                .unwrap_or(target_dir.to_str().unwrap().to_string());
+            let target_location = Path::new(&target_string);
+            let path = template.get_local_template_dir();
+            let git_ignore_str = read_gitignore(path);
+            let all_paths_to_read = get_all_in_dir(path, Some(git_ignore_str));
+            let parent_path = target_location.join(template.to_str());
+
+            if !target_location.exists() {
+                fs::create_dir(&target_location).unwrap();
             }
+
+            if target_location.is_dir() {
+                if !parent_path.exists() || !parent_path.is_dir() {
+                    fs::create_dir(&parent_path).unwrap();
+                }
+
+                if parent_path.is_file() {
+                    panic!("The thing is already file");
+                }
+            }
+
+            all_paths_to_read.iter().for_each(|x| {
+                let mut path = x.to_str().unwrap().split("/").skip(3).collect::<Vec<_>>();
+                path.pop();
+                let mut final_path = Path::new(target_location).join(template.to_str());
+
+                for each_thing in path {
+                    let temp_dir = final_path.join(each_thing);
+                    if !temp_dir.exists() {
+                        fs::create_dir(temp_dir).unwrap();
+                        final_path.push(each_thing);
+                    }
+                }
+
+                let path = x.to_str().unwrap();
+                let path = path.replace("./templates", target_location.to_str().unwrap());
+                let final_path = Path::new(&path);
+
+                let content = fs::read_to_string(x).unwrap();
+                fs::File::create(final_path).expect("file creation failed");
+                fs::write(final_path, content).expect("file write failed");
+            });
+            println!("Done writing")
         }
-        None => panic!("First argument action is not passed"),
-    };
-
-    let template = match template {
-        Some(item) => item,
-        None => panic!("Second argument cannot be empty"),
-    };
-
-    match action {
-        Action::New => match Templates::from_str(&template) {
-            Ok(template_enum) => {
-                let target_string = env::args()
-                    .nth(3)
-                    .unwrap_or(target_dir.to_str().unwrap().to_string());
-                let target_location = Path::new(&target_string);
-                let path = template_enum.get_local_template_dir();
-                let git_ignore_str = read_gitignore(path);
-                let all_paths_to_read = get_all_in_dir(path, Some(git_ignore_str));
-                let parent_path = target_location.join(template_enum.to_str());
-
-                if !target_location.exists() {
-                    fs::create_dir(&target_location).unwrap();
-                }
-
-                if target_location.is_dir() {
-                    if !parent_path.exists() || !parent_path.is_dir() {
-                        fs::create_dir(&parent_path).unwrap();
-                    }
-
-                    if parent_path.is_file() {
-                        panic!("The thing is already file");
-                    }
-                }
-
-                all_paths_to_read.iter().for_each(|x| {
-                    let mut path = x.to_str().unwrap().split("/").skip(3).collect::<Vec<_>>();
-                    path.pop();
-                    let mut final_path = Path::new(target_location).join(template_enum.to_str());
-
-                    for each_thing in path {
-                        let temp_dir = final_path.join(each_thing);
-                        if !temp_dir.exists() {
-                            fs::create_dir(temp_dir).unwrap();
-                            final_path.push(each_thing);
-                        }
-                    }
-
-                    let path = x.to_str().unwrap();
-                    let path = path.replace("./templates", target_location.to_str().unwrap());
-                    let final_path = Path::new(&path);
-
-                    let content = fs::read_to_string(x).unwrap();
-                    fs::File::create(final_path).expect("file creation failed");
-                    fs::write(final_path, content).expect("file write failed");
-                });
-                println!("Done writing")
-            }
-            Err(_e) => {
-                eprintln!("Invliad template")
-            }
-        },
-        Action::Ls => {}
-    };
+        Actions::Ls { template } => {
+            println!("Ls command for {template:?}")
+        }
+    }
 
     Ok(())
 }
