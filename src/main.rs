d06b17f91acb36a9d756268380bd94e4ab8dfb30
sairaj <sairaj2119@gmail.com>
2023-03-24 13:46:05 +0530

    bunch of changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
-    env,
     error::Error,
     fs, panic,
     path::{Path, PathBuf},
-    str::FromStr,
+    str::FromStr, collections::HashMap,
 };
+
+use rand::random;
 
 use clap::{Parser, Subcommand, ValueEnum};
 
@@ -15,10 +16,10 @@ enum Templates {
 }
 
 impl Templates {
-    fn get_local_template_dir(&self) -> &Path {
+    fn get_local_template_dir(&self) -> PathBuf {
         match self {
-            self::Templates::TsNode => Path::new("./templates/ts-node"),
-            self::Templates::TsExpress => Path::new("./templates/ts-express"),
+            self::Templates::TsNode => Path::new("./templates/ts-node").to_path_buf(),
+            self::Templates::TsExpress => Path::new("./templates/ts-express").to_path_buf(),
         }
     }
 
@@ -61,57 +62,69 @@ enum Actions {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let home_dir = dirs::home_dir()
+    let target_path = dirs::home_dir()
         .expect("Error while getting home dir")
         .join("playground");
 
-    if !home_dir.exists() {
-        fs::create_dir(&home_dir).expect("Error in creating the target dir")
+    if !target_path.exists() {
+        fs::create_dir(&target_path).expect("Error in creating the target dir")
     }
 
     let cli = Cli::parse();
 
     match cli.actions {
         Actions::New { template } => {
-            let target_string = env::args()
-                .nth(3)
-                .unwrap_or(home_dir.to_str().unwrap().to_string());
-            let target_location = Path::new(&target_string);
-            let template_dir_path = template.get_local_template_dir();
-            let git_ignore_str = read_gitignore(template_dir_path);
-            let all_paths_to_read = get_all_in_dir(template_dir_path, Some(git_ignore_str));
-            let parent_path = target_location.join(template.to_str());
+            let target_template_dir_path = target_path.join(template.to_str());
 
-            if !parent_path.exists() || !parent_path.is_dir() {
-                fs::create_dir(&parent_path).unwrap();
+            if !target_template_dir_path.exists() || !target_template_dir_path.is_dir() {
+                fs::create_dir(&target_template_dir_path).unwrap();
             }
 
-            if parent_path.is_file() {
-                panic!("The thing is already file");
+            let mut random_folder = template.to_str().to_string();
+            random_folder.push_str(&random::<u32>().to_string());
+
+            if !target_template_dir_path.join(&random_folder).exists() {
+                fs::create_dir(&target_template_dir_path.join(&random_folder))
+                    .expect("failed to create a random folder in target template dir");
             }
 
-            all_paths_to_read.iter().for_each(|x| {
-                let mut path = x.to_str().unwrap().split("/").skip(3).collect::<Vec<_>>();
-                path.pop();
-                let mut final_path = Path::new(target_location).join(template.to_str());
+            let local = template.get_local_template_dir();
+            let all_paths_to_read = get_all_in_dir(&local);
+            let all_paths_to_read = all_paths_to_read.iter().map(|x| x).collect::<Vec<_>>();
 
-                for each_thing in path {
-                    let temp_dir = final_path.join(each_thing);
-                    if !temp_dir.exists() {
-                        fs::create_dir(temp_dir).unwrap();
-                        final_path.push(each_thing);
-                    }
+
+
+            let new_all_paths = all_paths_to_read
+                .iter()
+                .map(|path| {
+                    let stripe = Path::new("./templates")
+                        .join(template.to_str())
+                        .to_path_buf();
+                    path.strip_prefix(stripe).unwrap().to_path_buf()
+                })
+                .map(|striped| target_template_dir_path.join(&random_folder).join(striped))
+                .collect::<Vec<_>>();
+
+            let mut map: HashMap<&PathBuf, &PathBuf> = HashMap::new();
+            
+            for (key, value) in new_all_paths.iter().zip(all_paths_to_read) {
+                map.insert(key, value);
+            }
+
+            new_all_paths.iter().for_each(|path| {
+                if !path.parent().unwrap().exists() {
+                    fs::create_dir_all(path.parent().unwrap()).unwrap();
                 }
 
-                let path = x.to_str().unwrap();
-                let path = path.replace("./templates", target_location.to_str().unwrap());
-                let final_path = Path::new(&path);
+                fs::File::create(&path).unwrap();
 
-                let content = fs::read_to_string(x).unwrap();
-                fs::File::create(final_path).expect("file creation failed");
-                fs::write(final_path, content).expect("file write failed");
+                let original_path = map.get(path).unwrap();
+                let contents = fs::read_to_string(original_path).unwrap();
+
+                fs::write(&path, contents).unwrap();
             });
-            println!("Done writing")
+
+            println!("Project created")
         }
         Actions::Ls { template } => {
             println!("Ls command for {template:?}");
@@ -121,15 +134,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn read_gitignore(path: &Path) -> String {
+
+fn read_gitignore(path: &PathBuf) -> String {
     let file = fs::read_to_string(path.join(".gitignore")).unwrap_or("".to_string());
     file.trim().to_string()
 }
 
-fn get_all_in_dir(path: &Path, ignore: Option<String>) -> Vec<PathBuf> {
+fn get_all_in_dir(path: &PathBuf) -> Vec<PathBuf> {
     let dir_contents = fs::read_dir(path).unwrap();
     let mut result: Vec<PathBuf> = vec![];
-    let git_ignore = ignore.unwrap_or(String::from(""));
+
+    let git_ignore = read_gitignore(&path);
 
     for item in dir_contents {
         let dir_entry = item.unwrap();
@@ -142,7 +157,7 @@ fn get_all_in_dir(path: &Path, ignore: Option<String>) -> Vec<PathBuf> {
         let meta = dir_entry.metadata().unwrap();
 
         if meta.is_dir() {
-            let more_results = get_all_in_dir(&path.join(&file_name), None);
+            let more_results = get_all_in_dir(&path.join(&file_name));
             result.extend(more_results);
         } else {
             result.push(dir_entry.path());
