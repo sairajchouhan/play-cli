a686b99e128c6aad9c4ecb6fd2eeff7ffe222b04
sairaj <sairaj2119@gmail.com>
2023-03-25 21:51:49 +0530

    something is happening for sure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use {
     clap::{Parser, Subcommand, ValueEnum},
-    std::{error::Error, fs, io, path::PathBuf, str::FromStr},
+    serde::{Deserialize, Serialize},
+    std::{collections::HashMap, error::Error, fs, io, path::PathBuf, str::FromStr},
 };
 
 #[derive(ValueEnum, Clone, Debug)]
@@ -65,25 +66,28 @@ enum Actions {
     },
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let target_path = dirs::home_dir()
-        .expect("Error while getting home dir")
-        .join("playground");
-
-    if !target_path.exists() {
-        fs::create_dir(&target_path).expect("Error in creating the target dir")
-    }
-
+fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
+
+    init_config().expect("config directory initialization failed");
+
+    let config = get_config();
+
+    let target_dir_path = config.target_dir;
+
+
+    if !target_dir_path.exists() {
+        fs::create_dir(&target_dir_path).expect("target dir creation failed")
+    }
 
     match cli.actions {
         Actions::New { template, name } => {
-            let has_template_root_dir = target_path.join(template.to_str()).exists();
+            let has_template_root_dir = target_dir_path.join(template.to_str()).exists();
             if !has_template_root_dir {
-                fs::create_dir(target_path.join(template.to_str())).unwrap();
+                fs::create_dir(target_dir_path.join(template.to_str())).unwrap();
             }
             let project_dir = name.unwrap_or(memorable_wordlist::snake_case(32));
-            let project_dir_path = target_path.join(template.to_str()).join(project_dir);
+            let project_dir_path = target_dir_path.join(template.to_str()).join(project_dir);
             copy_dir_recursive(&template.get_templates_dir(), &project_dir_path).unwrap();
         }
         Actions::Ls { template } => {
@@ -118,3 +122,42 @@ fn copy_dir_recursive(src_dir: &PathBuf, target_dir: &PathBuf) -> io::Result<()>
     }
     Ok(())
 }
+
+fn get_config_dir_path() -> PathBuf {
+    dirs::config_local_dir()
+        .expect("cannot get users local config dir")
+        .join("play-cli")
+        .join("play.json")
+}
+
+fn init_config() -> anyhow::Result<()> {
+    let user_local_config = dirs::config_local_dir().expect("cannot fine users local config dir");
+
+    if !user_local_config.join("play-cli").exists() {
+        fs::create_dir(user_local_config.join("play-cli"))?;
+    }
+
+    if !user_local_config
+        .join("play-cli")
+        .join("play.json")
+        .exists()
+    {
+        let temp = user_local_config.join("play-cli").join("play.json");
+        fs::File::create(&temp)?;
+        fs::write(temp, String::from("{}"))?;
+    }
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    target_dir: PathBuf,
+}
+
+fn get_config() -> Config {
+    let config_dir_path = get_config_dir_path();
+    let config_string = fs::read_to_string(config_dir_path).unwrap();
+    serde_json::from_str::<Config>(config_string.as_str()).expect("parsing json from string to Config failed")
+}
+
